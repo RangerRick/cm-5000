86a9016943129fad84a48ab4c3b49971b23f6dff
Benjamin Reed <ranger@opennms.org>
2019-04-02 15:37:09 -0400

    proof of concept beacon stuff

---
+++ src/3rdparty/ibeacon/README.md
@@ -0,0 +1,23 @@
+from https://github.com/IT-Dan/Ionic4ibeacon.git
+
+<a style="float:right;font-size:12px;" href="http://github.com/ionic-team/ionic-native/edit/master/src/@ionic-native/plugins/ibeacon/index.ts#L224">
+  Improve this doc
+</a>
+
+# IBeacon
+
+```
+$ ionic cordova plugin add cordova-plugin-ibeacon
+$ npm install @ionic-native/ibeacon
+```
+
+Install the plugins as normal. After you're done installing @ionic-native/ibeacon, you will need to replace two files.
+
+- index.js
+- ngx/index.js
+
+It's not a perfect solution, but it's working.
+
+Last tested 
+* Ionic (Ionic CLI) - 4.10.3
+* Cordova (Cordova CLI) - 8.1.2
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-import { HTTP } from '@ionic-native/http/ngx';
-import { IBeacon } from '@ionic-native/ibeacon/ngx';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { BeaconService } from './core/beacon.service';
 
 @Component({
   selector: 'app-root',
@@ -14,39 +11,15 @@ import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 })
 export class AppComponent {
   private idCount: number;
-  private http: HTTP;
-  private ibeacon: IBeacon;
-  private localNotifications: LocalNotifications;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private h: HTTP,
-    private ib: IBeacon,
-    private ln: LocalNotifications
+    private beaconService: BeaconService,
   ) {
     this.idCount = 0;
-    this.http = h;
-    this.ibeacon = ib;
-    this.localNotifications = ln;
     this.initializeApp();
-  }
-
-  rangeBeaconsInRegion(data) {
-    console.log('Range beacons in region: ' + JSON.stringify(data, undefined, 2));
-  }
-
-  startMonitoringForRegion(data) {
-    console.log('Start monitoring for region: ' + JSON.stringify(data, undefined, 2));
-  }
-
-  enterRegion(data) {
-    console.log('Enter region: ' + JSON.stringify(data, undefined, 2));
-  }
-
-  exitRegion(data) {
-    console.log('Exit region: ' + JSON.stringify(data, undefined, 2));
   }
 
   initializeApp() {
@@ -54,34 +27,6 @@ export class AppComponent {
       console.log('Initializing...');
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      this.http.useBasicAuth('demo', 'demo');
-
-      console.log('plugins: ' + Object.keys(window.cordova.plugins));
-
-      this.ibeacon.requestAlwaysAuthorization();
-      const delegate = this.ibeacon.Delegate();
-
-      /*
-      delegate.didRangeBeaconsInRegion().subscribe(this.rangeBeaconsInRegion, console.error);
-      delegate.didStartMonitoringForRegion().subscribe(this.startMonitoringForRegion, console.error);
-      delegate.didEnterRegion().subscribe(this.enterRegion, console.error);
-      delegate.didExitRegion().subscribe(this.exitRegion, console.error);
-      */
-
-      /*
-      window['evothings'].eddystone.startScan(beacon => {
-        console.log('Found beacon: ' + JSON.stringify(beacon, undefined, 2));
-        this.http.get('https://demo.opennms.org/opennms/rest/info', {}, {}).then(response => {
-          console.log('got response: ' + JSON.stringify(response.data, undefined, 2));
-          this.localNotifications.schedule({
-            title: 'Scan complete!',
-            text: 'Beacon "' + beacon.name + '" triggered an HTTP update!',
-          });
-        });
-      }, err => {
-        console.log('Error: ' + err);
-      });
-      */
     });
   }
 }
